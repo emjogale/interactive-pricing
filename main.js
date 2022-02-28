dae5c04f442af2e9a9149898fa7ec218a61933a4
Emma Gale <emmajgale@hotmail.com>
2022-02-28 16:10:07 +0000

    add in variable to check discount

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let pageviews = document.querySelector(".page-views span");
 let amount = document.querySelector("span.dollars");
 let discount = document.getElementById("yearly");
 let monthly = document.getElementById("monthly");
+
 let views;
 let num = 16;
 
